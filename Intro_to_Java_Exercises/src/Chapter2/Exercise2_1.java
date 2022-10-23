f7d0126b4f96c5c04828ef557086ce894a6ab135
Mark J. Cirlincione <72281636+MarkC35@users.noreply.github.com>
2022-10-23 14:57:06 -0400

    Update Exercise2_1.java

--- Intro_to_Java_Exercises/src/Chapter2/Exercise2_1.java
+++ Intro_to_Java_Exercises/src/Chapter2/Exercise2_1.java
@@ -1,13 +1,17 @@
 package Chapter2; 
+import java.util.Scanner;
 
 public class Exercise2_1 {
 
 	public static void main(String[] args) {
-		double i = 50.0;
-		double k = i + 50.0;
-		double j = k + 1;
+		Scanner input = new Scanner(System.in);
 		
-		System.out.println("j is " + j + " and k is " + k);
+		System.out.println("Enter miles: ");
+		double mile = input.nextDouble();
+		
+		double kilometer = mile * 1.6;
+		
+		System.out.println(mile + " mile(s) is " + kilometer + " kilometers");
 	}
 
 }
