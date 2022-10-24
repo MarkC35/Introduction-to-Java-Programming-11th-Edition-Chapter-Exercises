c7146b7e6b536b653c9d6b5135d506ecf8e761a4
Mark J. Cirlincione <72281636+MarkC35@users.noreply.github.com>
2022-10-24 14:54:47 -0400

    Add files via upload

---
+++ Intro_to_Java_Exercises/src/Chapter2/Exercise2_15.java
@@ -0,0 +1,25 @@
+package Chapter2;
+import java.util.Scanner;
+
+public class Exercise2_15 {
+
+	public static void main(String[] args) {
+		// Get user input for x1,y1 and x2,y2
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter x1 and y1: ");
+		double x1 = input.nextDouble();
+		double y1 = input.nextDouble();
+		
+		System.out.print("Enter x2 and y2: ");
+		double x2 = input.nextDouble();
+		double y2 = input.nextDouble();
+		
+		// Calculate the distance and display it
+		double distance = Math.pow(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2), 0.5);
+		
+		System.out.println("The distance between the two points is " + distance);
+
+	}
+
+}
