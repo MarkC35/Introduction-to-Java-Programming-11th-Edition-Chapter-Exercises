a0ca52f714b22e0ee326f68621a4504beeb83297
Mark J. Cirlincione <72281636+MarkC35@users.noreply.github.com>
2022-10-24 17:44:35 -0400

    Add files via upload

---
+++ Intro_to_Java_Exercises/src/Chapter2/Exercise2_18.java
@@ -0,0 +1,49 @@
+package Chapter2;
+
+public class Exercise2_18 {
+
+	public static void main(String[] args) {
+		//write a program that displays table and calculates the middle of two points
+		int x1, y1, x2, y2;
+		System.out.println("   a	    b	    Middle Point");
+		x1 = 0;
+		y1 = 0;
+		x2 = 2;
+		y2 = 1;
+		System.out.println("(" + x1 + ", " + y1 + ")    " + "(" + x2 + ", " + y2 + ")    " +
+	                "(" + (x1 + x2) / 2.0 + ", " + (y1 + y2) / 2.0 + ")");
+		
+		System.out.println("   a	    b	    Middle Point");
+		x1 = 1;
+		y1 = 4;
+		x2 = 4;
+		y2 = 2;
+		System.out.println("(" + x1 + ", " + y1 + ")    " + "(" + x2 + ", " + y2 + ")    " +
+	                "(" + (x1 + x2) / 2.0 + ", " + (y1 + y2) / 2.0 + ")");
+	
+		System.out.println("   a	    b	    Middle Point");
+		x1 = 2;
+		y1 = 7;
+		x2 = 6;
+		y2 = 3;
+		System.out.println("(" + x1 + ", " + y1 + ")    " + "(" + x2 + ", " + y2 + ")    " +
+	                "(" + (x1 + x2) / 2.0 + ", " + (y1 + y2) / 2.0 + ")");
+	
+		System.out.println("   a	    b	    Middle Point");
+		x1 = 3;
+		y1 = 9;
+		x2 = 10;
+		y2 = 5;
+		System.out.println("(" + x1 + ", " + y1 + ")    " + "(" + x2 + ", " + y2 + ")    " +
+	                "(" + (x1 + x2) / 2.0 + ", " + (y1 + y2) / 2.0 + ")");
+		
+		System.out.println("   a	    b	    Middle Point");
+		x1 = 4;
+		y1 = 11;
+		x2 = 12;
+		y2 = 7;
+		System.out.println("(" + x1 + ", " + y1 + ")   " + "(" + x2 + ", " + y2 + ")    " +
+	                "(" + (x1 + x2) / 2.0 + ", " + (y1 + y2) / 2.0 + ")");
+	}
+
+}
